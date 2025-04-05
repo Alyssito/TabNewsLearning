937504e23c216832b5283248ead82aaa227c48cc
ALYSSON SANTOS <160049428+Alyssito@users.noreply.github.com>
2025-04-05 02:09:55 +0000

    adiciona scritpts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Jantar Está pronto?</h1>;
 }
 
-function teste() {
-  console.log("Teste");
-}
-
-function teste2() {
-  console.log("identação errada.");
-}
-
 export default Home;
